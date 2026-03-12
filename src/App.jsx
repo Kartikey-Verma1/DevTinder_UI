99b71e16f61a7a7bf9bfb765c1849ace6837ff76
Kartikey-Verma1 <kartik.v7391@gmail.com>
2026-03-12 23:35:49 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import NavBar from "./components/NavBar"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/Login"
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Body />}>
+                <Route path="/login" element={<Login />} />
+            </ Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
